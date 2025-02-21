d74d552265eacefd23486a2193222c9fa2d7cc0d
SatyamRaj1905 <satyamraj1905@gmail.com>
2025-02-22 04:05:20 +0530

    generation of random color added

---
+++ Random-color-changer/script.js
@@ -0,0 +1,11 @@
+// Generate a random color
+
+const randomColor = () => {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
